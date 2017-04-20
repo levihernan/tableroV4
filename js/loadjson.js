a5fdaf9d7c77f2f98604a3a3491cd17a26eae37a
saresq <santiago.aresq@gmail.com>
2017-04-20 17:21:04 -0300

    data.json

---
+++ js/loadjson.js
@@ -0,0 +1,8 @@
+// $.getJSON("js/data.json", function(json) {
+// dataset = json;
+//     console.log(dataset); // this will show the info it in firebug consol
+//
+// });
+
+var json = require('data.json');
+console.log(json);
