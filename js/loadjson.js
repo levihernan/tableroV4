66586e39d323348598fee387c21e2623c28e1b43
levihernan <levi.hernan@gmail.com>
2017-04-21 16:37:43 -0300

    Ajax + Jason meeeeeein

--- js/loadjson.js
+++ js/loadjson.js
@@ -1,6 +1,24 @@
 var testAPI = "js/test.json";
 
-$.getJSON(testAPI, function(data) {
-  alert(data.name);
-  alert(data.categoria);
+// $.getJSON(testAPI, function(data) {
+//   // dataset = data
+// }, dataset = data);
+// alert(dataset.name)
+
+
+jQuery.ajax({
+    url: testAPI,
+    type: "GET",
+    dataType: "json",
+    contentType: "application/text; charset=utf-8",
+    success: function (data) {
+      alert('lalala')
+    },
+    error: function (response) {
+        console.log("failed API load");
+    }
+}).done(function(){
+    console.log(provincias_general_data);
+    load_map();
+    // loadDonutData();
 });
