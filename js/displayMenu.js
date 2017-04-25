091c75889f1c3fc5f6d07152bc88b79ad04e3b8c
levihernan <levi.hernan@gmail.com>
2017-04-25 16:03:30 -0300

    Menú is here motherfuckeeeers

--- js/displayMenu.js
+++ js/displayMenu.js
@@ -1,3 +1,11 @@
 function displayMenu(dataset) {
-  
+  tableData = '';
+  for (var i = 0; i < dataset.length; i++) {
+    tableData += '<ul class="collapsible collapsible-accordion"><li><a class="collapsible-header">' + dataset[i].categoria + '<i class="material-icons">arrow_drop_down</i></a><div class="collapsible-body"><ul>'
+    for (var j = 0; j < dataset[i].variables.length; j++) {
+      tableData += '<li><a class="waves-effect" href="#" onclick="">' + dataset[i].variables[j].nombre + '</a></li>'
+    }
+    tableData += '</ul></li></ul>'
+  }
+  $('#menuContent').append(tableData);
 }
