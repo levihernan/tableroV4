8e81b37faff0c4b9bab14714e1236ac4250fe725
saresq <santiago.aresq@gmail.com>
2017-04-25 17:49:38 -0300

    LastTimeSeen Beta-Table
    Soon to be killed

--- js/displayMenu.js
+++ js/displayMenu.js
@@ -1,7 +1,7 @@
 function displayMenu(dataset) {
   tableData = '';
   for (var i = 0; i < dataset.length; i++) {
-    tableData += '<ul class="collapsible collapsible-accordion"><li><a class="collapsible-header">' + dataset[i].categoria + '<i class="material-icons">arrow_drop_down</i></a><div class="collapsible-body"><ul>'
+    tableData += '<ul class="collapsible collapsible-accordion"><li><a class="collapsible-header blue-text">' + dataset[i].categoria + '<i class="material-icons">arrow_drop_down</i></a><div class="collapsible-body"><ul>'
     for (var j = 0; j < dataset[i].variables.length; j++) {
       tableData += '<li><a class="waves-effect" href="#" onclick="">' + dataset[i].variables[j].nombre + '</a></li>'
     }
