b2abda3b29f8545f53dba1fc63fd9330b27727dd
levihernan <levi.hernan@gmail.com>
2017-05-30 17:27:06 -0300

    mobile tabulate

--- js/tabulate.js
+++ js/tabulate.js
@@ -44,11 +44,35 @@ function tabulate(dataset) {
   $('#aclaraciones').append("<!-- FALTAN ACLARACIONES -->")
 }
 
+function mobileTabulate(dataset) {
+  tableData = '';
+  for (var i = 0; i < dataset.length; i++) {
+    tableRow = "<table class='mobileTable' id='"+ dataset[i].categoria +"'><tr class='mobileCategoria'><td>" + dataset[i].categoria + "</td></tr>";
+    for (var j = 0; j < dataset[i].variables.length; j++) {
+      tableRow = "<tbody class='mobileSerie'>";
+      tableRow += "<tr class='mobileIndicador'><td class='fullwidth'>" + dataset[i].variables[j].tablero.indicador + "</td></tr>";
+
+      tableRow += "<tr class='mobileData'><td class='halfwidth'>Unidad</td><td class='halfwidth'>" + dataset[i].variables[j].tablero.unidad + "</td></tr>";
+      tableRow += "<tr class='mobileTitulo'><td class='fullwidth'>Últimos datos</td></tr>";
+      tableRow += "<tr class='mobileData'><td class='halfwidth'>Período</td><td class='halfwidth'>" + dataset[i].variables[j].tablero.ultimoDato.periodo + "</td></tr>";
+      tableRow += "<tr class='mobileData'><td class='halfwidth'>Valor</td><td class='halfwidth'>" + dataset[i].variables[j].tablero.ultimoDato.valor + "</td></tr>";
+      tableRow += "<tr class='mobileData'><td class='halfwidth'>Variación IA</td><td class='halfwidth'>" + dataset[i].variables[j].tablero.ultimoDato.variacion + "</td></tr>";
+      tableRow += "<tr class='mobileTitulo'><td class='fullwidth'>Acumulado</td></tr>";
+      tableRow += "<tr class='mobileData'><td class='halfwidth'>Período</td><td class='halfwidth'>" + dataset[i].variables[j].tablero.acumulado.periodo + "</td></tr>";
+      tableRow += "<tr class='mobileData'><td class='halfwidth'>Valor</td><td class='halfwidth'>" + dataset[i].variables[j].tablero.acumulado.valor + "</td></tr>";
+      tableRow += "<tr class='mobileData'><td class='halfwidth'>Variación IA</td><td class='halfwidth'>" + dataset[i].variables[j].tablero.acumulado.variacion + "</td></tr></tbody>";
+    }
+    tableData += tableRow;
+  }
+    $('#mobileTable').append(tableData);
+
+}
+
 
 
 function toggleTablero(){
   if($('#tableroContainer').hasClass('hideContainer')){
-      $('#chartContainer').toggleClass('hideContainer');
-      $('#tableroContainer').toggleClass('hideContainer');
+    $('#chartContainer').toggleClass('hideContainer');
+    $('#tableroContainer').toggleClass('hideContainer');
   }
 };
